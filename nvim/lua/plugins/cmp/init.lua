ed88784008878f88553d57a4af3d73903ee4426d
Rodrigo Martín <martinbrodrigo@gmail.com>
2023-03-17 13:32:32 -0300

    add icons to lsp suggestions

--- nvim/lua/plugins/cmp/init.lua
+++ nvim/lua/plugins/cmp/init.lua
@@ -1,4 +1,33 @@
 local cmp = require('cmp')
+
+local kind_icons = {
+    Text = "",
+    Method = "m",
+    Function = "",
+    Constructor = "",
+    Field = "",
+    Variable = "",
+    Class = "",
+    Interface = "",
+    Module = "",
+    Property = "",
+    Unit = "",
+    Value = "",
+    Enum = "",
+    Keyword = "",
+    Snippet = "",
+    Color = "",
+    File = "",
+    Reference = "",
+    Folder = "",
+    EnumMember = "",
+    Constant = "",
+    Struct = "",
+    Event = "",
+    Operator = "",
+    TypeParameter = "",
+}
+
 cmp.setup({
     snippet = {
         expand = function(args)
@@ -10,21 +39,23 @@ cmp.setup({
         -- documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+            ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<C-e>'] = cmp.mapping.abort(),
+            ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     formatting = {
-        fields = {'kind', 'abbr', 'menu' },
+        -- Kind icons
+        fields = { 'kind', 'abbr', 'menu' },
         format = function(entry, vim_item)
+            vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
             vim_item.menu = ({
-                nvim_lsp = '[LSP]',
-                luasnip = '[Snippet]',
-                buffer = '[Buffer]',
-                path = '[Path]',
-            })[entry.source.name]
+                    nvim_lsp = '[LSP]',
+                    luasnip = '[Snippet]',
+                    buffer = '[Buffer]',
+                    path = '[Path]',
+                })[entry.source.name]
             return vim_item
         end
     },
@@ -54,7 +85,6 @@ cmp.setup.cmdline('/', {
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
---[[
 cmp.setup.cmdline(':', {
     mapping = cmp.mapping.preset.cmdline(),
     sources = cmp.config.sources({
@@ -63,4 +93,3 @@ cmp.setup.cmdline(':', {
         { name = 'cmdline' }
     })
 })
---]]
