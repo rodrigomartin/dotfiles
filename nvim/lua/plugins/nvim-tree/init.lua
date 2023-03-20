30ae8f2b72c7dd08545ef6e7a733286fb982b74e
Rodrigo Martín <martinbrodrigo@gmail.com>
2023-03-20 19:37:26 -0300

    fix: default dockerfile icons acts weird

--- nvim/lua/plugins/nvim-tree/init.lua
+++ nvim/lua/plugins/nvim-tree/init.lua
@@ -1,3 +1,21 @@
+require('nvim-web-devicons').setup {
+    override = {
+        ['dockerfile'] = {
+            icon = '',
+            name = 'Dockerfile',
+        },
+        ['docker-compose.yml'] = {
+            icon = '',
+            name = 'Dockerfile',
+        },
+        ['docker-compose.yaml'] = {
+            icon = '',
+            name = 'Dockerfile',
+        },
+    },
+    default = true,
+}
+
 require('nvim-tree').setup {
     actions = {
         open_file = {
